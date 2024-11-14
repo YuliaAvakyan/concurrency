26dc77aecc1c41bbdf40edce0323f325dd76295c
Yulia Avakyan <avakyanue@vtb.ru>
2024-11-14 10:31:34 +0300

    2. Асинхронные задачи CompletableFuture

--- src/main/java/course/concurrency/m2_async/minPrice/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/minPrice/PriceAggregator.java
@@ -1,11 +1,21 @@
 package course.concurrency.m2_async.minPrice;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class PriceAggregator {
+    private ExecutorService executor = Executors.newCachedThreadPool();
 
-    private PriceRetriever priceRetriever = new PriceRetriever();
+  private static final int DEFAULT_TIMEOUT = 2900;
+
+  private PriceRetriever priceRetriever = new PriceRetriever();
 
     public void setPriceRetriever(PriceRetriever priceRetriever) {
         this.priceRetriever = priceRetriever;
@@ -19,6 +29,33 @@ public class PriceAggregator {
 
     public double getMinPrice(long itemId) {
         // place for your code
-        return 0;
+
+      List<CompletableFuture<Double>> completableFutures = shopIds.stream()
+          .map(shopId -> CompletableFuture.supplyAsync(() -> priceRetriever.getPrice(shopId, itemId), executor)
+          .completeOnTimeout(Double.POSITIVE_INFINITY, 2900, TimeUnit.MILLISECONDS)
+          .exceptionally(ex -> Double.POSITIVE_INFINITY))
+          .toList();
+
+      CompletableFuture
+          .allOf(completableFutures.toArray(CompletableFuture[]::new))
+          .join();
+
+      return completableFutures
+          .stream()
+          .mapToDouble(CompletableFuture::join)
+          .filter(Double::isFinite)
+          .min()
+          .orElse(Double.NaN);
+
+//      return shopIds.stream()
+//          .map(shopId -> CompletableFuture
+//              .supplyAsync(() -> priceRetriever.getPrice(itemId, shopId), executor)
+//              .exceptionally(ex -> Double.POSITIVE_INFINITY)
+//              .completeOnTimeout(Double.POSITIVE_INFINITY, 2900, TimeUnit.MILLISECONDS)
+//          )
+//          .reduce((future1, future2) -> future1.thenCombine(future2, Double::min))
+//          .map(CompletableFuture::join)
+//          .filter(Double::isFinite)
+//          .orElse(Double.NaN);
     }
 }
