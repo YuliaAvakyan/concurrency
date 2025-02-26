06d8dad488720eeed8fbf2df3f425829bae97e9e
Yulia Avakyan <avakyanue@vtb.ru>
2025-02-26 20:03:31 +0300

    Блокировки 2 - 2

--- src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
+++ src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
@@ -1,9 +1,12 @@
 package course.concurrency.m3_shared.collections;
 
+import static java.util.stream.Collectors.toSet;
+
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.LongAdder;
 
 public class RestaurantService {
 
@@ -13,7 +16,8 @@ public class RestaurantService {
         put("C", new Restaurant("C"));
     }};
 
-    private Object stat;
+    private Map<String, Long> stat = new ConcurrentHashMap<>();
+//    private final Map<String, LongAdder> stat = new ConcurrentHashMap<>();
 
     public Restaurant getByName(String restaurantName) {
         addToStat(restaurantName);
@@ -21,11 +25,16 @@ public class RestaurantService {
     }
 
     public void addToStat(String restaurantName) {
+      stat.merge(restaurantName, 1L, (k,v) -> k + 1);
+//      stat.computeIfAbsent(restaurantName, k -> new LongAdder()).increment();
         // your code
     }
 
     public Set<String> printStat() {
         // your code
-        return new HashSet<>();
+        return stat.entrySet()
+            .stream()
+            .map(stringLongAdderEntry -> stringLongAdderEntry.getKey() + " - " + stringLongAdderEntry.getValue().longValue())
+            .collect(toSet());
     }
 }
